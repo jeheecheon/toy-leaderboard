d854a40af90def345baeca10f053a82e6c195170
jeheecheon <jeheecheon@gmail.com>
2024-11-07 01:41:50 +0900

    chore: 기존 main.tsx에서 router 설정까지 한 것 역할 분리
    main.tsx 에서 App.tsx 컴포넌트 추가 분리 및 라우팅 App에서 적용.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { browserRouter } from "@/utils/router";
 
 import "@/assets/styles/app.css";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <RouterProvider router={browserRouter} />
-  </StrictMode>
-);
+function App() {
+  return <RouterProvider router={browserRouter} />;
+}
+
+export default App;
