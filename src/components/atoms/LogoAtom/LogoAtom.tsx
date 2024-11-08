21046c0f48ffee0153f6b31546d13a814a076661
jeheecheon <jeheecheon@gmail.com>
2024-11-09 02:11:53 +0900

    fix: LogoAtom 컴포넌트 모바일에서 사이즈 잘못 출력되는 현상 수정
    모바일에서 LogoAtom 컴포넌트, 이미지 크기가 변형되어 출력되는 현상 수장
    npm run dev 시 내부 네트워크로 포트 포워딩 되도록 포트 개방되어 실행되도록 수정.

--- src/components/atoms/LogoAtom/LogoAtom.tsx
+++ src/components/atoms/LogoAtom/LogoAtom.tsx
@@ -1,7 +1,7 @@
 import zeroXAnd from "@/assets/images/zeroXAnd.png";
 
 function LogoAtom() {
-  return <img loading="eager" src={zeroXAnd} className="w-14 lg:w-16 h-fit" alt="ZeroXAnd Logo" />;
+  return <img loading="eager" src={zeroXAnd} className="w-14 lg:w-16 lg:h-fit" alt="ZeroXAnd Logo" />;
 }
 
 export default LogoAtom;
