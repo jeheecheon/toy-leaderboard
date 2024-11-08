a6cf680d61b0740f8bdd6632f99a454c7f8ec62c
jeheecheon <jeheecheon@gmail.com>
2024-11-08 18:17:11 +0900

    chore: tanstack query devtools 설치
    모듈 설치: @tanstack/react-query-devtools
    App에 provider 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { RouterProvider } from "react-router-dom";
 import { browserRouter } from "@/utils/router";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/utils/tanstack";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import "@/assets/styles/app.css";
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={browserRouter} />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
